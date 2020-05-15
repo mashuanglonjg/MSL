ecf82e92928f7edcea508af24dc9b10ad8e9ba24
wancheng <525918717@qq.com>
2020-05-15 10:27:20 +0800

    新增服务协议及用例

--- page/login.py
+++ page/login.py
@@ -4,6 +4,7 @@
 @Desc    : 登录页
 '''
 from poium import Page, PageElement
+from page.main import Mainpage
 
 class Loginpage(Page):
     sms_bt = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[1]/div[1]/span')  # 切换手机号输入
@@ -14,6 +15,10 @@ class Loginpage(Page):
     findpwd_bt = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div[4]/span')  # 找回密码
     findpwd_title = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[1]/strong')  # 找回密码标题
     back_bt = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[1]/span')  # 返回
+    service_bt = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[3]/em')  # 服务协议
+    service_title = PageElement(xpath='/html/body/div[2]/div/div[1]/strong')  # 服务协议title
+    service_text = PageElement(class_name='ccp-custom-modal-body')  # 服务协议文本
+
 
 
     def findpwd(self):
@@ -27,9 +32,21 @@ class Loginpage(Page):
         return str(self.findpwd_title.text)
 
     def back(self):
+        """返回"""
         self.back_bt.click()
 
+    def service(self):
+        """点击服务协议"""
+        self.get("https://webapp.leke.cn/lt-web/index.html#/login")
+        self.service_bt.click()
 
+    def get_service_title(self):
+        """获取服务协议标题"""
+        return str(self.service_title.text)
+
+    def get_service_text(self):
+        """获取服务协议文本"""
+        return str(self.service_text.text)
 
     def login(self,loginname,password):
         """登录"""
@@ -38,4 +55,5 @@ class Loginpage(Page):
         self.loginname_loc = loginname
         self.password_loc = password
         self.login_loc.click()
+        return Mainpage(self.driver)
 
--- test_case/test_login.py
+++ test_case/test_login.py
@@ -8,7 +8,7 @@ import pytest
 from page.login import Loginpage
 import allure
 
-@allure.feature('demo')
+@allure.feature('乐桃学院')
 class TestLogin():
     login_page = Loginpage(browser)
     def test_findpwd(self,browser):
@@ -18,12 +18,17 @@ class TestLogin():
         assert '重置密码' in login_page.get_findpwd_title()
         login_page.back()
 
-    
+    def test_service(self,browser):
+        """检查服务协议内容"""
+        login_page = Loginpage(browser)
+        login_page.service()
+        assert '乐桃学院服务协议' in login_page.get_service_title()
+        assert 'hello world' in login_page.get_service_text()
+
     def test_login(self,browser):
         """登录"""
         login_page = Loginpage(browser)
         login_page.login('955112', 'test1234')
-
 
 
 if __name__ == '__main__':
