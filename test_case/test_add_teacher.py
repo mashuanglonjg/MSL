fb813a838b798d92e5f8a4cf170ccfad2892aa8e
fengzhj <15850512966@163.com>
2020-05-19 10:57:51 +0800

    update

--- test_case/test_add_teacher.py
+++ test_case/test_add_teacher.py
@@ -16,12 +16,13 @@ class TestAddTC():
 
     def test_addpage(self,browser):
         """是否进入添加老师页面"""
-        add_page = Loginpage(browser).admin_login('2512759', 'test1234').add_tc()
+        add_page = Loginpage(browser).admin_login('18888888888', 'test1234').add_tc()
         assert '添加老师' == add_page.get_add_title()
 
     def test_add_tc(self,browser):
         """单个添加老师"""
-        add_page = Loginpage(browser).admin_login('2512759', 'test1234').add_tc()
+        # add_page = Loginpage(browser).admin_login('2512759', 'test1234').add_tc()
+        add_page = Loginpage(browser).admin_login('18888888888', 'test1234').add_tc()
         add_page.add_teacher('18877777777', '自动化测试', '这是UI自动化数据, 这是UI自动化数据')
         assert '自动化测试' == add_page.get_tc_name()
 
