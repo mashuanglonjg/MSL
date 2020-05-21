b72dd9cff90b1408aca4281975e3e908fde53d31
fengzhj <15850512966@163.com>
2020-05-21 19:48:05 +0800

    更新页面

--- page/login.py
+++ page/login.py
@@ -6,6 +6,7 @@
 from poium import Page, PageElement
 from page.admin_manger import Adminpage
 from page.usercenter import Usercenter_page
+import time
 
 class Loginpage(Page):
     sms_bt = PageElement(xpath='/html/body/div[1]/div/div[2]/div/div/div[1]/div[1]/span')  # 切换手机号输入
@@ -65,6 +66,7 @@ class Loginpage(Page):
         self.loginname_loc = loginname
         self.password_loc = password
         self.login_loc.click()
+        time.sleep(3)
         return Adminpage(self.driver)
 
     def user_login(self, loginname, password):
@@ -74,5 +76,6 @@ class Loginpage(Page):
         self.loginname_loc = loginname
         self.password_loc = password
         self.login_loc.click()
+        time.sleep(3)
         return Usercenter_page(self.driver)
 
